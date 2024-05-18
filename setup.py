143a7570eea9feeadd0d6531cfb815530ce5af8d
alice <dothinh.dev@gmail.com>
2024-05-18 16:44:43 +0700

    update toml

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='alice-connector',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A SQL connector developed by alice',
